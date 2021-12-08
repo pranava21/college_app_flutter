b970ca54c12f94a8f063484cc924ee4cc812e4f9
Pranava <th4274@365sg.xyz>
2021-12-08 22:04:40 +0530

    changed host and port

--- lib/endpoint.dart
+++ lib/endpoint.dart
@@ -2,8 +2,8 @@ import 'dart:core';
 
 class Endpoint {
   static const apiScheme = "http";
-  static const apiHost = '192.168.0.205';
-  static const apiPort = 7103;
+  static const apiHost = '192.168.0.170';
+  static const apiPort = 80;
 
   static Uri uri(String path, {required Map<String, dynamic> queryParameters}) {
     final uri = Uri(
