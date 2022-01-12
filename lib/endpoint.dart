d592ec5c11e2421b29c4492d71c1e614d8094f66
Pranava <th4274@365sg.xyz>
2022-01-12 15:31:49 +0530

    Changed port and added ssl

--- lib/endpoint.dart
+++ lib/endpoint.dart
@@ -1,9 +1,9 @@
 import 'dart:core';
 
 class Endpoint {
-  static const apiScheme = "http";
+  static const apiScheme = "https";
   static const apiHost = 'pranava.zapto.org';
-  static const apiPort = 80;
+  static const apiPort = 443;
 
   static Uri uri(String path, {required Map<String, dynamic> queryParameters}) {
     final uri = Uri(
